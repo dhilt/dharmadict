3de2e3e61c887e38cfd20b2f742e5bf96f8cd74c
dhilt <d.alexanov@gmail.com>
2017-03-27 23:42:04 +0300

    initial state refactoring

---
+++ app/reducers/_initial.js
@@ -0,0 +1,50 @@
+import auth from '../helpers/auth'
+
+let initialState = {
+  route: {
+    location: null
+  },
+  auth: {
+    loggedIn: false,
+    token: null,
+    userInfo: {
+      requested: false,
+      pending: false,
+      promise: null,
+      data: null,
+      error: null
+    },
+    modalIsOpen: false,
+    login: '',
+    password: '',
+    pending: false,
+    error: null
+  },
+  search: {
+    searchString: '',
+    started: false,
+    pending: false,
+    result: null,
+    error: null
+  },
+  selected: {
+    term: null
+  },
+  edit: {
+    started: false,
+    termId: '',
+    termName: '',
+    source: null,
+    change: null,
+    pending: false,
+    error: null,
+    update: {
+      pending: false,
+      error: null
+    }
+  }
+}
+
+auth.initialize(initialState.auth)
+
+export default initialState
