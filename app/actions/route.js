829a2715924d49f2101b4c838f3274803561c4f1
dhilt <d.alexanov@gmail.com>
2017-03-29 00:56:12 +0300

    back history search state improvement

--- app/actions/route.js
+++ app/actions/route.js
@@ -1,18 +1,19 @@
-import {selectTerm} from './search'
-
-import {
-  CHANGE_ROUTE
-} from './_constants'
+import { selectTerm, selectTermAsync } from './search'
+import { CHANGE_ROUTE } from './_constants'
 
 export function changeRoute(location) {
   return (dispatch, getState) => {
-    let state = getState()
-    let prevLocation = state.route.location
-    if(prevLocation && prevLocation.query.term !== location.query.term) {
-      if(state.selected.term.id !== location.query.term) {
-        let term = state.search.result.find(term => term.id === location.query.term)
-        if(term) {
-          dispatch(selectTerm(term, true))
+    if (location.pathname.length <= 1) {
+      let state = getState()
+      let prevLocation = state.route.location
+      if (prevLocation && prevLocation.query.term !== location.query.term) {
+        if (state.selected.term.id !== location.query.term) {
+          let term = state.search.result.find(term => term.id === location.query.term)
+          if (term) {
+            dispatch(selectTerm(term, true))
+          } else {
+            dispatch(selectTermAsync(location.query.term))
+          }
         }
       }
     }
