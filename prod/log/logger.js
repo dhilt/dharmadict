8cab54bbffe91e2b787b4b1664d98dc14f62c2a4
dhilt <d.alexanov@gmail.com>
2017-09-06 03:13:16 +0300

    do not log to file in TEST environment

--- prod/log/logger.js
+++ prod/log/logger.js
@@ -1,15 +1,30 @@
-var winston = require('winston');
+const winston = require('winston');
 
-var logger = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({ json: false, timestamp: true }),
-    new winston.transports.File({ filename: __dirname + '/debug.log', json: false })
-  ],
-  exceptionHandlers: [
-    new (winston.transports.Console)({ json: false, timestamp: true }),
-    new winston.transports.File({ filename: __dirname + '/exceptions.log', json: false })
-  ],
-  exitOnError: false
-});
+let logger;
+
+if (process.env.NODE_ENV !== 'test') {
+  logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({json: false, timestamp: true}),
+      new winston.transports.File({filename: __dirname + '/debug.log', json: false})
+    ],
+    exceptionHandlers: [
+      new (winston.transports.Console)({json: false, timestamp: true}),
+      new winston.transports.File({filename: __dirname + '/exceptions.log', json: false})
+    ],
+    exitOnError: false
+  });
+}
+else {
+  logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({json: false, timestamp: true})
+    ],
+    exceptionHandlers: [
+      new (winston.transports.Console)({json: false, timestamp: true})
+    ],
+    exitOnError: false
+  });
+}
 
 module.exports = logger;
