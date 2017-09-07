179fc100cca93c01614f8baedb8c540e9c0b6477
dhilt <d.alexanov@gmail.com>
2017-09-07 20:04:19 +0300

    tests: create user, login, delete user

---
+++ test/createUserSpec.js
@@ -0,0 +1,65 @@
+const assert = require('assert');
+const testAdmin = require('./_shared.js').testAdmin;
+
+const usersController = require('../prod/controllers/users.js');
+
+describe('Create user ctrl method', () => {
+
+  it('should not create user (no data)', (done) => {
+    usersController.create()
+      .then(result => done(new Error('User was created')))
+      .catch(error => {
+        assert.equal(error.text, 'Invalid data');
+        done();
+      })
+  });
+
+  const shouldNotCreateUser = (invalidKey) => {
+    it('should not create user (no ' + invalidKey + ')', (done) => {
+      let user = Object.assign({}, testAdmin);
+      delete user[invalidKey];
+      usersController.create(user)
+        .then(result => done(new Error('User was created')))
+        .catch(error => {
+          assert.equal(error.text, 'Invalid ' + invalidKey);
+          done();
+        })
+    });
+  };
+
+  shouldNotCreateUser('id');
+  shouldNotCreateUser('name');
+  shouldNotCreateUser('role');
+  shouldNotCreateUser('login');
+  shouldNotCreateUser('password');
+
+  it('should create user', (done) => {
+    usersController.create(testAdmin)
+      .then(result => {
+        assert.equal(result.success, true);
+        setTimeout(() => done(), 1500);
+      })
+      .catch(error => done(error))
+  });
+
+  it('should not create the same (by login) user', (done) => {
+    usersController.create(testAdmin)
+      .then(result => done(new Error('User was created')))
+      .catch(error => {
+        assert.equal(error.text, 'Login not unique');
+        done();
+      })
+  });
+
+  it('should not create the same (by id) user', (done) => {
+    let user = Object.assign({}, testAdmin);
+    user.login = user.login + 'NEW';
+    usersController.create(user)
+      .then(result => done(new Error('User was created')))
+      .catch(error => {
+        assert.equal(error.text, 'Id not unique');
+        done();
+      })
+  });
+
+});
