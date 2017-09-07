b6b5af8005158ef3e53d34dc546710636e7a9ea7
dhilt <d.alexanov@gmail.com>
2017-09-07 23:09:52 +0300

    force cleanup test term

--- test/_shared.js
+++ test/_shared.js
@@ -7,53 +7,64 @@ process.env.NODE_ENV = 'test';
 
 const server = require('../prod/server.js');
 const usersController = require('../prod/controllers/users.js');
+const termsController = require('../prod/controllers/terms.js');
 
 chai.use(chaiHttp);
 const request = chai.request(server);
 
 const testAdmin = {
-  "id": "TEST-ADMIN",
-  "role": "admin",
-  "login": "test-admin",
-  "name": "Test Admin",
-  "description": "...",
-  "password": "test-admin-pass"
+  id: "TEST-ADMIN",
+  role: "admin",
+  login: "test-admin",
+  name: "Test Admin",
+  description: "...",
+  password: "test-admin-pass"
 };
 
 const testTranslator = {
-  "id": "TEST-TRANSLATOR",
-  "role": "translator",
-  "login": "test-translator",
-  "name": "Test Translator",
-  "description": "...",
-  "password": "test-translator-pass"
+  id: "TEST-TRANSLATOR",
+  role: "translator",
+  login: "test-translator",
+  name: "Test Translator",
+  description: "...",
+  password: "test-translator-pass"
+};
+
+const testTerm = {
+  name: 'test term',
+  id: 'test_term'
 };
 
 const forceCleanUp = () => {
   describe('Force cleanup', () => {
     it('may delete test user', (done) => {
-
       let ready = 0;
-      const _done = (index) => {
-        if(ready) {
+      const _done = () => {
+        if (++ready === 3) {
           done();
         }
-        ready = index;
       };
 
       usersController.removeById(testAdmin.id)
         .then(() => {
           console.log('Test admin user was successfully deleted');
-          setTimeout(() => _done(1), 1000);
+          setTimeout(() => _done(), 1000);
         })
-        .catch(() => _done(1));
+        .catch(() => _done());
 
       usersController.removeById(testTranslator.id)
         .then(() => {
           console.log('Test translator user was successfully deleted');
-          setTimeout(() => _done(2), 1000);
+          setTimeout(() => _done(), 1000);
         })
-        .catch(() => _done(2));
+        .catch(() => _done());
+
+      termsController.removeById(testTerm.id)
+        .then(() => {
+          console.log('Test term user was successfully deleted');
+          setTimeout(() => _done(), 1000);
+        })
+        .catch(() => _done());
     });
   });
 };
@@ -80,5 +91,6 @@ module.exports = {
   request,
   testAdmin,
   testTranslator,
+  testTerm,
   shouldLogIn
 };
