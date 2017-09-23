00669a12af1d42d1b6f495f5ee8d4a5bff1604e2
BitDen <yuvzhenko@inbox.ru>
2017-09-23 16:43:55 +0800

    Refactore code in tests

--- test/searchTermSpec.js
+++ test/searchTermSpec.js
@@ -29,6 +29,36 @@ describe('Search term API', () => {
     )
   });
 
+  testTranslationsInTerm = (translations, term, testTermTranslation) => {
+    // Testing 'translations' in 'term'
+    assert.equal(true, !!translations);
+    assert.notEqual(undefined, translations.length);
+    assert.equal(true, translations.length === 2);
+    const _firstTranslation = translations.find(elem => elem.translatorId === testTranslator.id);
+    const _secondTranslation = translations.find(elem => elem.translatorId === testTranslator2.id);
+    assert.equal(true, !!_firstTranslation);
+    assert.equal(true, !!_secondTranslation);
+    assert.equal(testTranslator.language, _firstTranslation.language);
+    assert.equal(testTranslator2.language, _secondTranslation.language);
+    // Continue testing 'translations' in 'term'. Now, "meanings"
+    const _meanings = _firstTranslation.meanings;
+    assert.equal(true, !!_meanings);
+    assert.notEqual(undefined, _meanings.length);
+    assert.equal(true, _meanings.length === 2);
+    _meanings.forEach(meaning => {
+      assert.equal(true, !!meaning.comment || meaning.comment === null);
+      assert.equal(true, !!meaning.versions);
+      assert.equal(true, !!meaning.versions_lower);
+    });
+    for (let i = 0; i < _meanings.length; i++) {
+      for (let j = 0; j < _meanings[i].versions.length; j++) {
+        assert.equal(_meanings[i].versions[j], testTermTranslation.translation.meanings[i].versions[j]);
+        assert.equal(_meanings[i].versions_lower[j], testTermTranslation.translation.meanings[i].versions[j].toLowerCase());
+        assert.equal(_meanings[i].comment, testTermTranslation.translation.meanings[i].comment);
+      }
+    }
+  };
+
   it('should search two test terms', (done) => {
     request.get('/api/search?pattern=' + testTerm.name).end(
       (err, res) => {
@@ -44,62 +74,10 @@ describe('Search term API', () => {
         assert.equal(testTerm2.id, _secondTerm.id);
 
         // Here testing translations in first term
-        const _firstTermTranslations = _firstTerm.translations;
-        assert.equal(true, !!_firstTermTranslations);
-        assert.notEqual(undefined, _firstTermTranslations.length);
-        assert.equal(true, _firstTermTranslations.length === 2);
-        let _firstTranslation = _firstTermTranslations.find(elem => elem.translatorId === testTranslator.id);
-        let _secondTranslation = _firstTermTranslations.find(elem => elem.translatorId === testTranslator2.id);
-        assert.equal(true, !!_firstTranslation);
-        assert.equal(true, !!_secondTranslation);
-        assert.equal(testTranslator.language, _firstTranslation.language);
-        assert.equal(testTranslator2.language, _secondTranslation.language);
-        // Continue testing translations in first term. Now, "meanings"
-        let _meanings = _firstTranslation.meanings;
-        assert.equal(true, !!_meanings);
-        assert.notEqual(undefined, _meanings.length);
-        assert.equal(true, _meanings.length === 2);
-        _meanings.forEach(meaning => {
-          assert.equal(true, !!meaning.comment || meaning.comment === null);
-          assert.equal(true, !!meaning.versions);
-          assert.equal(true, !!meaning.versions_lower);
-        });
-        for (let i = 0; i < _meanings.length; i++) {
-          for (let j = 0; j < _meanings[i].versions.length; j++) {
-            assert.equal(_meanings[i].versions[j], testTermTranslation.translation.meanings[i].versions[j]);
-            assert.equal(_meanings[i].versions_lower[j], testTermTranslation.translation.meanings[i].versions[j].toLowerCase());
-            assert.equal(_meanings[i].comment, testTermTranslation.translation.meanings[i].comment);
-          }
-        }
+        testTranslationsInTerm(_firstTerm.translations, testTerm, testTermTranslation);
 
         // Here testing translations in second term
-        const _secondTermTranslations = _secondTerm.translations;
-        assert.equal(true, !!_secondTermTranslations);
-        assert.notEqual(undefined, _secondTermTranslations.length);
-        assert.equal(true, _secondTermTranslations.length === 2);
-        _firstTranslation = _secondTermTranslations.find(elem => elem.translatorId === testTranslator.id);
-        _secondTranslation = _secondTermTranslations.find(elem => elem.translatorId === testTranslator2.id);
-        assert.equal(true, !!_firstTranslation);
-        assert.equal(true, !!_secondTranslation);
-        assert.equal(testTranslator.language, _firstTranslation.language);
-        assert.equal(testTranslator2.language, _secondTranslation.language);
-        // Continue testing translations in second term. Now, "meanings"
-        _meanings = _firstTranslation.meanings;
-        assert.equal(true, !!_meanings);
-        assert.notEqual(undefined, _meanings.length);
-        assert.equal(true, _meanings.length === 2);
-        _meanings.forEach(meaning => {
-          assert.equal(true, !!meaning.comment || meaning.comment === null);
-          assert.equal(true, !!meaning.versions);
-          assert.equal(true, !!meaning.versions_lower);
-        });
-        for (let i = 0; i < _meanings.length; i++) {
-          for (let j = 0; j < _meanings[i].versions.length; j++) {
-            assert.equal(_meanings[i].versions[j], testTermTranslation2.translation.meanings[i].versions[j]);
-            assert.equal(_meanings[i].versions_lower[j], testTermTranslation2.translation.meanings[i].versions[j].toLowerCase());
-            assert.equal(_meanings[i].comment, testTermTranslation2.translation.meanings[i].comment);
-          }
-        }
+        testTranslationsInTerm(_secondTerm.translations, testTerm2, testTermTranslation2);
         done();
       }
     )
@@ -118,33 +96,7 @@ describe('Search term API', () => {
         assert.equal(_term.id, testTerm2.id);
 
         // Here testing translations in term
-        const _termTranslations = _term.translations;
-        assert.equal(true, !!_termTranslations);
-        assert.notEqual(undefined, _termTranslations.length);
-        assert.equal(true, _termTranslations.length === 2);
-        let _firstTranslation = _termTranslations.find(elem => elem.translatorId === testTranslator.id);
-        let _secondTranslation = _termTranslations.find(elem => elem.translatorId === testTranslator2.id);
-        assert.equal(true, !!_firstTranslation);
-        assert.equal(true, !!_secondTranslation);
-        assert.equal(testTranslator.language, _firstTranslation.language);
-        assert.equal(testTranslator2.language, _secondTranslation.language);
-        // Continue testing translations in term. Now, "meanings"
-        let _meanings = _firstTranslation.meanings;
-        assert.equal(true, !!_meanings);
-        assert.notEqual(undefined, _meanings.length);
-        assert.equal(true, _meanings.length === 2);
-        _meanings.forEach(meaning => {
-          assert.equal(true, !!meaning.comment || meaning.comment === null);
-          assert.equal(true, !!meaning.versions);
-          assert.equal(true, !!meaning.versions_lower);
-        });
-        for (let i = 0; i < _meanings.length; i++) {
-          for (let j = 0; j < _meanings[i].versions.length; j++) {
-            assert.equal(_meanings[i].versions[j], testTermTranslation2.translation.meanings[i].versions[j]);
-            assert.equal(_meanings[i].versions_lower[j], testTermTranslation2.translation.meanings[i].versions[j].toLowerCase());
-            assert.equal(_meanings[i].comment, testTermTranslation2.translation.meanings[i].comment);
-          }
-        }
+        testTranslationsInTerm(_term.translations, testTerm2, testTermTranslation2);
         done();
       }
     )
