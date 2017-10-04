31f2b124fa1ce5c8b7060ede81d7f8a9da67aa6c
BitDen <yuvzhenko@inbox.ru>
2017-10-04 12:57:55 +0800

    Removed routes to app/routes.js

--- app/index.js
+++ app/index.js
@@ -9,22 +9,13 @@ import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
 import ConnectedIntlProvider from './ConnectedIntlProvider'
 
-import App from './components/App'
-import Home from './components/Home'
-import About from './components/About'
-import NotFound from './components/NotFound'
-import Edit from './components/Edit'
-import TranslatorPage from './components/TranslatorPage'
-import NewTerm from './components/admin/NewTerm'
-import EditUser from './components/admin/EditUser'
-
 import reducer from './reducers'
 import {changeRoute} from './actions/route'
 import {getUserInfoAsync} from './actions/auth'
 import {getCommonDataAsync} from './actions/common'
 import {selectTermAsync} from './actions/search'
 
-// import Routes from './routes'
+import routes from './routes'
 import './styles/main.css'
 import './styles/images/favicon.ico';
 
@@ -53,63 +44,6 @@ if(location.query.term) {
 browserHistory.listenBefore((location) => {
   return store.dispatch(changeRoute(location))
 })
-
-function unauthorizedAccess (replace) {
-  replace('/not_authorized')
-  browserHistory.replace('/not_authorized')
-}
-
-function unpermittedAccess (replace) {
-  replace('/not_permitted')
-  browserHistory.replace('/not_permitted')
-}
-
-function checkAuth (nextState, replace, callback, role) {
-  const auth = store.getState().auth
-  if(auth.loggedIn && (!role || (auth.userInfo.data && auth.userInfo.data.role === role))) {
-    callback()
-    return
-  }
-  if(auth.userInfo.promise) {
-    auth.userInfo.promise.then(response => {
-      if (!auth.loggedIn) {
-        unauthorizedAccess(replace)
-      }
-      if (role && (!response || !response.result || response.result.role !== role)) {
-        unpermittedAccess(replace)
-      }
-      callback()
-    })
-  }
-  else {
-    unauthorizedAccess(replace)
-  }
-}
-
-const routes = {
-  component: App,
-  childRoutes: [
-    { path: '/', exactly: true, component: Home },
-    { path: '/about', exactly: true, component: About },
-    { path: '/edit', exactly: true, component: Edit },
-    {
-      path: '/newTerm',
-      exactly: true,
-      component: NewTerm,
-      onEnter: (...args) => checkAuth(...args, 'admin')
-    },
-    { path: '/translators/:id', exactly: true, component: TranslatorPage },
-    {
-      path: '/translators/:id/edit',
-      exactly: true,
-      component: EditUser,
-      onEnter: (...args) => checkAuth(...args, 'admin')
-    },
-    { path: '/not_authorized', exactly: true, component: NotFound },
-    { path: '/not_permitted', exactly: true, component: NotFound },
-    { path: '*', component: NotFound }
-  ]
-};
 
 ReactDOM.render(
   <Provider store={store}>
