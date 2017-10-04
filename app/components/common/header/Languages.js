8ef66dcb2b3fb1a05a23c67b39408d48ef7b9c76
BitDen <yuvzhenko@inbox.ru>
2017-10-04 13:27:11 +0800

    Rewrited styles for dropdown

--- app/components/common/header/Languages.js
+++ app/components/common/header/Languages.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {FormattedMessage} from 'react-intl'
+import {DropdownButton, MenuItem} from 'react-bootstrap'
 
 class Languages extends Component {
 
@@ -14,16 +15,13 @@ class Languages extends Component {
     const {isMenuOpen} = this.state
 
     return (
-      <span className="dropdown" onClick={this.toggleMenu}>
-        <FormattedMessage id="Header.change_language" />
-        <ul className="dropdown-menu" style={{display: isMenuOpen ? 'block' : 'none'}}>
-        {
-          languages && languages.length && languages.map(lang =>
-            <li key={lang.id} onClick={() => doChangeLang(lang.id)}>{lang.name}</li>
-          )
-        }
-        </ul>
-      </span>
+      <DropdownButton bsStyle={'info'} title={<FormattedMessage id="Header.change_language" />}>
+      {
+        languages && languages.length && languages.map(lang =>
+          <li key={lang.id} onClick={() => doChangeLang(lang.id)}>{lang.name}</li>
+        )
+      }
+      </DropdownButton>
     )
   }
 
