366dcb3c7fef38ae94d10e845a35bce7fc0b4dba
BitDen <yuvzhenko@inbox.ru>
2017-10-13 20:25:41 +0800

    Start write test on search-actions

---
+++ test/frontend/actions/search.spec.js
@@ -0,0 +1,59 @@
+const configureMockStore = require('redux-mock-store').default;
+const thunk = require('redux-thunk').default;
+const nock = require('nock');
+const expect = require('expect');
+
+const actions = require('../../../app/actions/search');
+const types = require('../../../app/actions/_constants');
+const reducer = require('../../../app/reducers').default;
+const initialState = require('../../../app/reducers/_initial').default;
+const lang = require('../../../app/helpers/lang').default;
+
+const translators = require('../_shared.js').translators;
+const terms = require('../_shared.js').terms;
+
+let middlewares = [thunk];
+let mockStore = configureMockStore(middlewares);
+
+describe('common actions', () => {
+  beforeEach(() => {
+    nock.disableNetConnect();
+    nock.enableNetConnect('127.0.0.1');
+    console.log = jest.fn();
+  });
+
+  afterEach(() => {
+    nock.cleanAll();
+    nock.enableNetConnect();
+  });
+
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('should change search string', () => {
+    let newSearchString = 'newSearchString';
+    const expectedAction = [{
+      type: types.CHANGE_SEARCH_STRING,
+      newSearchString
+    }];
+
+    // test reducers
+    let expectedState = JSON.parse(JSON.stringify(initialState));
+    expectedState.search.searchString = newSearchString;
+    expect(reducer(initialState, expectedAction[0])).toEqual(expectedState);
+
+    // test actions
+    let store = mockStore(initialState);
+    store.dispatch(actions.changeSearchString(newSearchString));
+    expect(store.getActions()).toEqual(expectedAction);
+  });
+
+  it('should correctly work. Function: doSearchRequestAsync', () => {
+    // ...
+  });
+
+  it('should correctly handle error. Function: doSearchRequestAsync', () => {
+    // ...
+  });
+})
