9a68112c71d2febaf7e78e50c47a67d7d6f69a8c
BitDen <yuvzhenko@inbox.ru>
2017-10-24 05:31:13 +0800

    Added App.spec

---
+++ test/frontend/components/App.spec.js
@@ -0,0 +1,21 @@
+global.window.localStorage = {};
+const React = require('react');
+const {Component} = require('react');
+const {expect} = require('chai');
+
+const App = require('../../../app/components/App').default;
+const {setupComponent} = require('../_shared.js');
+
+describe('Testing App Component.', () => {
+
+  it('should show component', () => {
+    const {wrapper} = setupComponent(App);
+
+    expect(wrapper.find('div.wrapper')).to.exist;
+    expect(wrapper.find('div.container')).to.exist;
+    expect(wrapper.find('div.alert-column')).to.exist;
+    expect(wrapper.find('div.nav')).to.exist;
+    expect(wrapper.find('div.nav.nav__wrapper')).to.exist;
+    expect(wrapper.find('div.nav.nav__wrapper.languages-bar-header')).to.exist;
+  });
+});
