79537442b9a9d3e04a525d1dc30dbec9720f601d
BitDen <yuvzhenko@inbox.ru>
2017-10-31 16:37:28 +0800

    New error-notifier

--- app/helpers/notifier.js
+++ app/helpers/notifier.js
@@ -1,14 +1,27 @@
 import {notify} from '../actions/notifier'
 
 const notifier = {
+  // onErrorResponse: (error, values = {}) => {
+  //   if(error) {
+  //     if(typeof error !== 'string') {
+  //       error = error.message
+  //     }
+  //     if(typeof error !== 'string') {
+  //       error = 'Common.unknown_error'
+  //     }
+  //     return notify({type: 'danger', text: error, ttl: -1, values})
+  //   }
+  // },
+
   onErrorResponse: (error, values = {}) => {
-    if(error) {
-      if(typeof error !== 'string') {
-        error = error.message
-      }
-      if(typeof error !== 'string') {
-        error = 'Common.unknown_error'
-      }
+    if(error.message) {  // if this is a server-error
+      values = { error: error.message }
+      error = 'Common.error_message'
+      return notify({type: 'danger', text: error, ttl: -1, values})
+    } else if (typeof error === 'string') {
+      return notify({type: 'danger', text: error, ttl: -1, values})
+    } else {
+      error = 'Common.unknown_error'
       return notify({type: 'danger', text: error, ttl: -1, values})
     }
   },
