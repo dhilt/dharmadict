e22ac2a593a14b7b6054e0613c5e00907c10b6c9
BitDen <yuvzhenko@inbox.ru>
2017-11-01 16:36:56 +0800

    Rewrited SearchResults with new style

--- test/frontend/components/search/SearchInput.spec.js
+++ test/frontend/components/search/SearchInput.spec.js
@@ -6,63 +6,69 @@ const {setupComponent, checkWrap, initialState, languages} = require('../../_sha
 
 describe('Testing SearchInput Component.', () => {
 
-  const checkShowSearchInput = (searchString, pending, lang) => {
-    const _initialState = { ...initialState,
-      common: { ...initialState.common,
-        userLanguage: lang
-      },
-      search: { ...initialState.search,
-        searchString,
-        pending
-      }
-    };
-    const wrapper = setupComponent(SearchInput, _initialState);
-    const i18n = require('../../../../app/i18n/' + lang);
+  const checkShowSearchInput = (searchString, pending) => {
+    languages.forEach(lang => {
+      const _initialState = { ...initialState,
+        common: { ...initialState.common,
+          userLanguage: lang.id
+        },
+        search: { ...initialState.search,
+          searchString,
+          pending
+        }
+      };
+      const wrapper = setupComponent(SearchInput, _initialState);
+      const i18n = require('../../../../app/i18n/' + lang.id);
 
-    checkWrap(wrapper.find('[data-test-id="SearchInput"]'), {
-      className: 'row'
-    });
+      checkWrap(wrapper.find('[data-test-id="SearchInput"]'), {
+        className: 'row'
+      });
 
-    checkWrap(wrapper.find('[data-test-id="main-form"]'));
+      checkWrap(wrapper.find('[data-test-id="main-form"]'));
 
-    checkWrap(wrapper.find('[data-test-id="form-group1"]'), {
-      className: 'form-group'
-    });
+      checkWrap(wrapper.find('[data-test-id="form-group1"]'), {
+        className: 'form-group'
+      });
 
-    checkWrap(wrapper.find('[data-test-id="form-group1.col-md-6"]'), {
-      className: 'col-md-6'
-    });
+      checkWrap(wrapper.find('[data-test-id="form-group1.col-md-6"]'), {
+        className: 'col-md-6'
+      });
 
-    checkWrap(wrapper.find('[data-test-id="form-group1.input"]'), {
-      className: 'form-control col-md-7',
-      name: 'search',
-      type: 'search',
-      value: searchString
-    });
+      checkWrap(wrapper.find('[data-test-id="form-group1.input"]'), {
+        className: 'form-control col-md-7',
+        name: 'search',
+        type: 'search',
+        value: searchString
+      });
 
-    checkWrap(wrapper.find('[data-test-id="div.col-md-2"]'), {
-      className: 'col-md-2'
-    });
+      checkWrap(wrapper.find('[data-test-id="div.col-md-2"]'), {
+        className: 'col-md-2'
+      });
 
-    checkWrap(wrapper.find('[data-test-id="form-group2"]'), {
-      className: 'form-group'
-    });
+      checkWrap(wrapper.find('[data-test-id="form-group2"]'), {
+        className: 'form-group'
+      });
 
-    checkWrap(wrapper.find('button[data-test-id="searchButton"]'), {
-      disabled: !searchString || pending,
-      className: pending ? 'loader' : '',
-      type: 'submit'
-    });
+      checkWrap(wrapper.find('button[data-test-id="searchButton"]'), {
+        disabled: !searchString || pending,
+        className: pending ? 'loader' : '',
+        type: 'submit'
+      });
 
-    checkWrap(wrapper.find('[data-test-id="button-pending"]'), {
-      text: i18n['SearchInput.button_find'],
-      className: pending ? 'invisible' : ''
+      checkWrap(wrapper.find('[data-test-id="button-pending"]'), {
+        text: i18n['SearchInput.button_find'],
+        className: pending ? 'invisible' : ''
+      });
     });
   };
 
-  languages.forEach(lang => {
-    it('should show component with initial empty data', () => checkShowSearchInput('', false, lang.id));
-    it('should show component with term searching', () => checkShowSearchInput('term', true, lang.id));
-    it('should show component ready for term searching', () => checkShowSearchInput('term', false, lang.id));
-  });
+  it('should show component with initial empty data',
+    () => checkShowSearchInput('', false)
+  );
+  it('should show component with term searching',
+    () => checkShowSearchInput('term', true)
+  );
+  it('should show component ready for term searching',
+    () => checkShowSearchInput('term', false)
+  );
 });
--- test/frontend/components/search/SearchResults.spec.js
+++ test/frontend/components/search/SearchResults.spec.js
@@ -1,12 +1,8 @@
 global.window.localStorage = {};
-const React = require('react');
-const {Component} = require('react');
 const {expect} = require('chai');
 
 const SearchResults = require('../../../../app/components/search/SearchResults').default;
-const {setupComponent, defaultLang, initialState, terms} = require('../../_shared.js');
-
-const i18n = require('../../../../app/i18n/' + defaultLang);
+const {setupComponent, checkWrap, languages, initialState, terms} = require('../../_shared.js');
 
 describe('Testing SearchResults Component.', () => {
 
@@ -15,83 +11,93 @@ describe('Testing SearchResults Component.', () => {
   };
 
   const checkShowSearchResults = (result, selectedTerm, pending, error) => {
-    const _initialState = { ...initialState,
-      search: { ...initialState.search,
-        started: true,
-        result,
-        error,
-        pending
-      },
-      selected: { ...initialState.selected,
-        term: selectedTerm
+    languages.forEach(lang => {
+      const _initialState = { ...initialState,
+        common: { ...initialState.common,
+          userLanguage: lang.id
+        },
+        search: { ...initialState.search,
+          started: true,
+          result,
+          error,
+          pending
+        },
+        selected: { ...initialState.selected,
+          term: selectedTerm
+        }
+      };
+      const wrapper = setupComponent(SearchResults, _initialState);
+      const i18n = require('../../../../app/i18n/' + lang.id);
+
+      checkWrap(wrapper.find('[data-test-id="SearchResults"]'), {
+        className: 'row search-results-row'
+      });
+
+      if (error) {
+        checkWrap(wrapper.find('[data-test-id="error"]'));
+        checkWrap(wrapper.find('[data-test-id="div-result"]'), { length: 0 });
       }
-    };
-    const wrapper = setupComponent(SearchResults, _initialState);
 
-    expect(wrapper.find('[data-test-id="SearchResults"]').length).equal(1);
-    expect(wrapper.find('[data-test-id="SearchResults"]').hasClass('row')).equal(true);
-    expect(wrapper.find('[data-test-id="SearchResults"]').hasClass('search-results-row')).equal(true);
-
-    if (error) {
-      expect(wrapper.find('[data-test-id="error"]').length).equal(1);
-      expect(wrapper.find('[data-test-id="div-result"]').length).equal(0);
-    }
-
-    if (!pending && !result) {
-      expect(wrapper.find('[data-test-id="not-found"]').length).equal(1);
-      expect(wrapper.find('[data-test-id="not-found"]').text()).equal(i18n['SearchResults.Not_found']);
-      expect(wrapper.find('[data-test-id="div-result"]').length).equal(0);
-    }
-
-    if (!pending && result) {
-      expect(wrapper.find('[data-test-id="error"]').length).equal(0);
-      expect(wrapper.find('[data-test-id="not-found"]').length).equal(0);
-
-      expect(wrapper.find('[data-test-id="div-result"]').length).equal(1);
-
-      expect(wrapper.find('[data-test-id="div-result.col-md-3"]').length).equal(1);
-      expect(wrapper.find('[data-test-id="div-result.col-md-3"]').hasClass('col-md-3')).equal(true);
-
-      expect(wrapper.find('[data-test-id="div-result.list"]').length).equal(1);
-      expect(wrapper.find('[data-test-id="div-result.list"]').hasClass('list-group')).equal(true);
-      expect(wrapper.find('[data-test-id="div-result.list"]').hasClass('terms')).equal(true);
-
-      expect(wrapper.find('[data-test-id="TermList"]').length).equal(1);
-      // further tests in "test/frontend/components/search/TermList"
-
-      expect(wrapper.find('[data-test-id="selectedTerm"]').length).equal(1);
-      expect(wrapper.find('[data-test-id="selectedTerm"]').hasClass('col-md-9')).equal(true);
-
-      if (selectedTerm) {
-        expect(wrapper.find('[data-test-id="Term"]').length).equal(1);
-        expect(wrapper.find('[data-test-id="Term"]').hasClass('term')).equal(true);
-        // further tests in "test/frontend/components/search/Term"
+      if (!pending && !result) {
+        checkWrap(wrapper.find('[data-test-id="not-found"]'), {
+          text: i18n['SearchResults.Not_found']
+        });
+        checkWrap(wrapper.find('[data-test-id="div-result"]'), { length: 0 });
       }
-    }
+
+      if (!pending && result) {
+        checkWrap(wrapper.find('[data-test-id="error"]'), { length: 0 });
+        checkWrap(wrapper.find('[data-test-id="not-found"]'), { length: 0 });
+
+        checkWrap(wrapper.find('[data-test-id="div-result"]'));
+
+        checkWrap(wrapper.find('[data-test-id="div-result.col-md-3"]'), {
+          className: 'col-md-3'
+        });
+
+        checkWrap(wrapper.find('[data-test-id="div-result.list"]'), {
+          className: 'list-group terms'
+        });
+
+        checkWrap(wrapper.find('[data-test-id="TermList"]'));
+        // further tests in "test/frontend/components/search/TermList"
+
+        checkWrap(wrapper.find('[data-test-id="selectedTerm"]'), {
+          className: 'col-md-9'
+        });
+
+        if (selectedTerm) {
+          checkWrap(wrapper.find('[data-test-id="Term"]'), {
+            className: 'term'
+          })
+          // further tests in "test/frontend/components/search/Term"
+        }
+      }
+    });
   };
 
   const result = terms;
   const selectedTerm = terms[1];
 
-  it('should show component with initial empty data', () =>
-    checkShowSearchResults(null, null, false, null)
+  it('should show component with initial empty data',
+    () => checkShowSearchResults(null, null, false, null)
   );
-  it('should show component with term searching', () =>
-    checkShowSearchResults(null, null, true, null)
+  it('should show component with term searching',
+    () => checkShowSearchResults(null, null, true, null)
   );
-  it('should show component with success request, data received', () =>
-    checkShowSearchResults(result, null, false, null)
+  it('should show component with success request, data received',
+    () => checkShowSearchResults(result, null, false, null)
   );
-  it('should show component with success request, data received and term selected', () =>
-    checkShowSearchResults(result, selectedTerm, false, null)
+  it('should show component with success request, data received and term selected',
+    () => checkShowSearchResults(result, selectedTerm, false, null)
   );
-  it('should show component with success request, data not received', () =>
-    checkShowSearchResults(null, null, false, null)
+  it('should show component with success request, data not received',
+    () => checkShowSearchResults(null, null, false, null)
   );
-  it('should show component with error request', () =>
-    checkShowSearchResults(null, null, false, responseFail)
+  it('should show component with error request',
+    () => checkShowSearchResults(null, null, false, responseFail)
   );
-  it('should show component with error request and starting new request', () =>
-    checkShowSearchResults(null, null, true, responseFail)
+  it('should show component with error request and starting new request',
+    () => checkShowSearchResults(null, null, true, responseFail)
   );
 });
