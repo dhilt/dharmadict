6728cd2e97e9b90b18572f593d3580b58011a2c9
BitDen <yuvzhenko@inbox.ru>
2017-12-03 16:23:33 +0800

    Splitted shared.js on needfull import and useless

--- test/frontend/_shared.js
+++ test/frontend/_shared.js
@@ -1,17 +1,35 @@
 global.window.localStorage = {};
 
-module.exports = Object.assign({}, {
+module.exports = {
+  shallowWithIntl: require('./__test-methods/mountTestedComponent').shallowWithIntl,
+  mountWithStore: require('./__test-methods/mountTestedComponent').mountWithStore,
+  mountWithIntl: require('./__test-methods/mountTestedComponent').mountWithIntl,
+  shallow: require('./__test-methods/mountTestedComponent').shallow,
   getNotificationAction: require('./__test-methods/getNotificationAction'),
-  defaultLang: require('../../app/helpers/lang').default.defaultLang,  // probably can be removed
-  initialState: require('../../app/reducers/_initial').default,
   cloneState: require('./__test-methods/cloneInitialState'),
+
+  defaultTranslator: require('./__test-data/mockUsers').defaultTranslator,
+  defaultUser: require('./__test-data/mockUsers').defaultUser,
+  admin: require('./__test-data/mockUsers').admin,
+  defaultTerm: require('./__test-data/mockTerms')[0],
+
+  defaultLang: require('../../prod/helper').languages.getLang().id,
+  initialState: require('../../app/reducers/_initial').default,
   getLang: require('../../prod/helper').languages.getLang,
   languages: require('../../prod/helper').languages.data,
-  lang: require('../../app/helpers/lang').default,  // probably can be removed
-  terms: require('./__test-data/mockTerms'),
   _appPath: '../../../../app/',
-  appPath: '../../../app/'
-},
-  require('./__test-methods/mountTestedComponent'),
-  require('./__test-data/mockUsers')
-);
+  appPath: '../../../app/',
+
+  // all code below should be removed
+  checkWrapActions: require('./__test-methods/mountTestedComponent').checkWrapActions,
+  setupComponent: require('./__test-methods/mountTestedComponent').setupComponent,
+  checkWrap: require('./__test-methods/mountTestedComponent').checkWrap,
+  newStore: require('./__test-methods/mountTestedComponent').newStore,
+
+  userMutableProperties: require('./__test-data/mockUsers').userMutableProperties,
+  roles: require('./__test-data/mockUsers').roles,
+  translators: require('./__test-data/mockUsers').translators,
+  users: require('./__test-data/mockUsers').users,
+
+  terms: require('./__test-data/mockTerms')
+};
