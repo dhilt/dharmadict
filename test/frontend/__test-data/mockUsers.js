7e1431654ad7f5bc2b4c501f4d9e266d6bce1352
BitDen <yuvzhenko@inbox.ru>
2017-12-03 16:10:46 +0800

    Planning removed useless massive array with users/translators. And added defaultUser and defaultTranslator

--- test/frontend/__test-data/mockUsers.js
+++ test/frontend/__test-data/mockUsers.js
@@ -1,16 +1,13 @@
 const languages = require('../../../prod/helper').languages.data;
+const defaultLang = require('../../../prod/helper').languages.getLang();
 
+// probably can be removed
 const roles = ['user', 'translator', 'admin'];
 
+// probably can be removed
 const userMutableProperties = ['name', 'description', 'language'];
 
-const admin = {
-  id: 'ADMIN_ID',
-  name: 'Admin name',
-  role: 'admin',
-  language: languages[0].id
-};
-
+// probably can be removed
 const translators = [{
   id: 'ZAG',
   name: 'Б.И. Загуменнов',
@@ -71,8 +68,33 @@ languages.forEach(lang => {
   });
 });
 
+const admin = {
+  id: 'ADMIN_ID',
+  name: 'Admin name',
+  role: 'admin',
+  language: defaultLang
+};
+
+const defaultUser = {
+  id: 'USER_ID',
+  name: 'Name of default user',
+  role: 'user',
+  description: 'description of user',
+  language: defaultLang
+};
+
+const defaultTranslator = {
+  id: 'TRANSLATOR_ID',
+  name: 'Name of default translator',
+  role: 'translator',
+  description: 'description of translator',
+  language: defaultLang
+};
+
 module.exports = {
   userMutableProperties,
+  defaultTranslator,
+  defaultUser,
   roles,
   admin,
   translators,
