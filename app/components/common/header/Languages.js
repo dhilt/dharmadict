3a3e35c38f3d283e707a051c0c625ae546124cff
BitDen <yuvzhenko@inbox.ru>
2017-12-11 12:26:41 +0800

    ...and added new Languages.spec.js

--- app/components/common/header/Languages.js
+++ app/components/common/header/Languages.js
@@ -1,27 +1,25 @@
 import React, {Component} from 'react'
-import {FormattedMessage} from 'react-intl'
 import {DropdownButton, MenuItem} from 'react-bootstrap'
+
 import lang from '../../../helpers/lang'
 
 class Languages extends Component {
 
   constructor (props) {
     super(props)
-    this.toggleMenu = this.toggleMenu.bind(this)
-    this.state = {isMenuOpen: false}
   }
 
   render () {
     const {languages, current, doChangeLang} = this.props
-    const {isMenuOpen} = this.state
 
     if(!languages || !languages.length) {
       return (null)
     }
 
     const showLangId = (langId) => (
-      <span data-test-id="Languages.showLangId" className={lang.get(langId) === current ? 'selected': ''}>
-        {lang.get(langId)}
+      <span data-test-id="Languages.showLangId"
+        className={lang.get(langId) === current ? 'selected': ''}
+      >{lang.get(langId)}
       </span>
     )
 
@@ -39,22 +37,22 @@ class Languages extends Component {
 
     return (
       <div data-test-id="Languages" className="languages-bar-header">
-        <DropdownButton data-test-id="Languages.Dropdown" title={title} pullRight id={`languagesDropdown`}>
+        <DropdownButton data-test-id="Languages.Dropdown"
+          id={`languagesDropdown`}
+          title={title}
+          pullRight>
         {
           languages.map(item =>
-            <MenuItem data-test-id="Languages.MenuItem" key={item.id} onSelect={() => doChangeLang(item.id)}>
-              {showLangId(item.id)} - {item.name}
+            <MenuItem data-test-id="Languages.MenuItem"
+              onSelect={() => doChangeLang(item.id)}
+              key={item.id}
+            >{showLangId(item.id)} - {item.name}
             </MenuItem>
           )
         }
         </DropdownButton>
       </div>
     )
-  }
-
-  toggleMenu(event) {
-    event.preventDefault()
-    this.setState({isMenuOpen: !this.state.isMenuOpen})
   }
 }
 
---
+++ test/frontend/components/common/header/Languages.spec.js
@@ -0,0 +1,85 @@
+const React = require('react');
+const {expect} = require('chai');
+const sinon = require('sinon');
+
+const {
+  mountWithIntl,
+  defaultLang,
+  languages,
+  _appPath,
+  shallow
+} = require('../../../_shared.js');
+
+const Languages = require('../' + _appPath + 'components/common/header/Languages').default;
+
+describe('Testing Languages Component.', () => {
+
+  const defaultEvent = {
+    preventDefault: () => true
+  };
+  const props = {
+    doChangeLang: sinon.spy(),
+    current: defaultLang,
+    languages
+  };
+
+  const LanguagesMenuItemId = '[data-test-id="Languages.MenuItem"]';
+
+  it('should correctly handle actions', () => {
+    const wrapper = shallow(<Languages {...props} />);
+
+    languages.forEach((lang, i) => {
+      wrapper.find(LanguagesMenuItemId).at(i).simulate('select', defaultEvent);
+    });
+
+    expect(props.doChangeLang.callCount).equal(languages.length);
+
+    wrapper.unmount();
+  });
+
+  const LanguagesId = '[data-test-id="Languages"]';
+  const dropdownBtnId = '[data-test-id="Languages.Dropdown"]';
+  const showLangId = '[data-test-id="Languages.showLangId"]';
+  const langItemId = '[data-test-id="Languages.langItem"]';
+  const titleId = '[data-test-id="Languages.title"]';
+
+  it('should show component correctly', () => {
+    const wrapper = mountWithIntl(<Languages {...props} />);
+
+    expect(wrapper.find(LanguagesId).exists()).equal(true);
+    expect(wrapper.find(dropdownBtnId).exists()).equal(true);
+
+    languages.forEach((lang, i) => {
+      wrapper.setProps({...props,
+        current: lang.id
+      });
+
+      expect(wrapper.find(titleId).exists()).equal(true);
+      const expectedTitleText = languages.reduce((init, elem, index) =>
+        init += elem.id + (index < languages.length - 1 ? '/' : ''), ''
+      );
+      expect(wrapper.find(titleId).text()).equal(expectedTitleText);
+
+      expect(wrapper.find(langItemId).at(i).exists()).equal(true);
+      const expectedLangItemText = lang.id + (i < languages.length - 1 ? '/' : '');
+      expect(wrapper.find(langItemId).at(i).text()).equal(expectedLangItemText);
+
+      languages.forEach((_lang, _i) => {
+        expect(wrapper.find(langItemId).at(_i).find(showLangId).exists()).equal(true);
+        expect(wrapper.find(langItemId).at(_i).find(showLangId).text()).equal(_lang.id);
+        expect(wrapper.find(langItemId).at(_i).find(showLangId).prop('className'))
+          .equal(lang.id === _lang.id ? 'selected' : '');
+
+        expect(wrapper.find(LanguagesMenuItemId).find(showLangId).at(_i).text()).equal(_lang.id);
+        expect(wrapper.find(LanguagesMenuItemId).find(showLangId).at(_i).prop('className'))
+          .equal(lang.id === _lang.id ? 'selected' : '');
+
+        const countOfItems = wrapper.find(LanguagesMenuItemId).length / languages.length;
+        expect(wrapper.find(LanguagesMenuItemId).at(_i * countOfItems).text())
+          .equal(_lang.id + ' - ' + _lang.name);
+      });
+    });
+
+    wrapper.unmount();
+  });
+});
