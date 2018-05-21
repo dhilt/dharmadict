c24721e9b1cb2ab0f7f979cbb892857da41cb5b1
dhilt <d.alexanov@gmail.com>
2018-05-21 12:30:58 +0300

    add_descriptions_for_akt_and_mk migration

--- prod/db/migrate.js
+++ prod/db/migrate.js
@@ -1,17 +1,18 @@
 const client = require('./client.js');
 
 let scripts = [
-  '20170908160000_cleanup',
+  /*'20170908160000_cleanup',
   '20170908161900_create_index',
   '20170908174900_add_terms',
   '20170908184600_add_users',
   '20170908205600_add_translations',
   '20180125205500_add_pages',
   '20180202050300_add_users_descriptions',
-  '20180501073100_add_tengon.js'
+  '20180501073100_add_tengon.js',*/
+  '20180521121500_add_descriptions_for_akt_and_mk.js'
 ];
 
-console.log('DB reset scripts running');
+console.log('DB migration scripts running');
 
 scripts = scripts.map(scriptName => require('./scripts/' + scriptName));
 
